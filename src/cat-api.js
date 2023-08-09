c77f6ff7584aad073a0b3580afd0f8e3a006d6fb
JemJoestar <jemapro2@gmail.com>
2023-08-10 01:06:42 +0300

    refactoring

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,10 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
+const BASIC_URL = 'https://api.thecatapi.com/v1/images/search';
+
 export function fetchBreed() {
-  fetch(`https://api.thecatapi.com/v1/breeds`)
+  return fetch(`https://api.thecatapi.com/v1/breeds`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
@@ -10,73 +12,31 @@ export function fetchBreed() {
 
       return response.json();
     })
-    .then(breedsArr => {
-      refs.selectionEl.innerHTML = createMarcup(breedsArr);
-    })
-    .catch(err => {
-      Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    });
+    
 }
 
-const BASIC_URL = 'https://api.thecatapi.com/v1/images/search';
-
-const refs = {
-  selectionEl: document.querySelector('.breed-select'),
-  loaderEl: document.querySelector('.loader'),
-  catInfoEl: document.querySelector('.cat-info'),
-};
-
-function createMarcup(breedsArr) {
-  return breedsArr
-    .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
-    .join('');
-}
 
 export function fetchCatByBreed(breedId) {
   Loading.arrows('Searching...', {
     svgColor: 'rgba(0,0,0,0.8)',
     backgroundColor: 'rgba(0,0,0,0.5)',
-    messageColor: 'rgba(0,0,0,0.8)'
+    messageColor: 'rgba(0,0,0,0.8)',
+    messageFontSize: "24px"
   });
   const searchParams = new URLSearchParams({
     breed_ids: breedId,
     api_key:
       'live_gpoZcOghIKxFU1uMBR9tGdAfF3nNIng6LpmcpEWwPGEdpQn6Ifd1p3v5eNwEUfsf',
   });
-  fetch(`${BASIC_URL}?${searchParams}`)
+  return fetch(`${BASIC_URL}?${searchParams}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
       }
       return response.json();
     })
-    .then(arr => (refs.catInfoEl.innerHTML = createCatImg(arr)))
-    .catch(err => {
-      Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    });
+    
+    ;
 }
 
-function createCatImg(breed) {
-    Loading.remove();
-  if (breed.length === 0) {
-    throw new Error();
-  }
-  return breed
-    .map(item => {
-      const {
-        breeds: {
-          [0]: { description, name, temperament },
-        },
-      } = item;
 
-      return `<img class="cat-photo" src="${item.url}" alt="${name}" width="300">
-        <div class="cat-info-conteiner">
-        <h1 class="cat-name">${name}</h1>
-        <p class="cat-description"> 
-         ${description}</p>
-        <p calss="cat-description"><span class="descr-title">Temperament: </span> ${temperament}</span</p>
-        </div>
-        `;
-    })
-    .join('');
-}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,58 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchBreed, fetchCatByBreed } from './cat-api.js';
-
-fetchBreed();
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
 const refs = {
   breedSelectEl: document.querySelector('.breed-select'),
   catInfoEl: document.querySelector('.cat-info'),
 };
 
+fetchBreed()
+  .then(breedsArr => (refs.breedSelectEl.innerHTML = createMarcup(breedsArr)))
+  .catch(err => {
+    Notify.failure('Oops! Something went wrong! Try reloading the page!');
+  });
 
 refs.breedSelectEl.addEventListener('change', findCatInfo);
 
 function findCatInfo(event) {
-
   refs.catInfoEl.innerHTML = '';
   const breed = event.currentTarget.value;
-  
+
   fetchCatByBreed(breed)
-  
+    .then(arr => (refs.catInfoEl.innerHTML = createCatImg(arr)))
+    .catch(err => {
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    });
 }
 
+function createMarcup(breedsArr) {
+  return breedsArr
+    .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
+    .join('');
+}
+
+function createCatImg(breed) {
+  Loading.remove();
+  if (breed.length === 0) {
+    throw new Error();
+  }
+  return breed
+    .map(item => {
+      const {
+        breeds: {
+          [0]: { description, name, temperament },
+        },
+      } = item;
+
+      return `<img class="cat-photo" src="${item.url}" alt="${name}" width="300">
+      <div class="cat-info-conteiner">
+      <h1 class="cat-name">${name}</h1>
+      <p class="cat-description"> 
+       ${description}</p>
+      <p calss="cat-description"><span class="descr-title">Temperament: </span> ${temperament}</span</p>
+      </div>
+      `;
+    })
+    .join('');
+}
